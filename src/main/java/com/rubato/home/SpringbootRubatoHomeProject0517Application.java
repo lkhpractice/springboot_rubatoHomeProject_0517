5a5809e74f3a5f45c83d4fcaba1f4e1fa1b27319
ici <ici@DESKTOP-CL6HLKL>
2023-05-17 11:14:18 +0900

    백업

---
+++ src/main/java/com/rubato/home/SpringbootRubatoHomeProject0517Application.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRubatoHomeProject0517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRubatoHomeProject0517Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubato/home/SpringbootRubatoHomeProject0517ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRubatoHomeProject0517ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
